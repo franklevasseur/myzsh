cb634ccf111c90d816c20c47b828b43f9b1e4f18
franklevasseur <francois_levasseur@hotmail.com>
2021-12-01 10:51:30 -0500

    small cleanup

--- bp.plugin.zsh
+++ bp.plugin.zsh
@@ -3,90 +3,34 @@ code="$HOME/Documents/code/"
 bot="$HOME/Documents/botpress-root/"
 bp_sql_uri="postgres://postgres:postgres@localhost:5432/botpress"
 bp_cache="$HOME/Library/ApplicationSupport/botpress"
+bp_zsh=${0:a}
 
-bp_main_pkg="bp_main"
-bp_core_pkg="botpress"
-
-bot() {
-    script="
-        const cliSelect = require('cli-select')
-        const fs = require('fs')
-        const path = require('path')
-
-        const bot = '$bot'
-
-        const childDirs = fs.readdirSync(bot, { withFileTypes: true })
-            .filter(dirent => dirent.isDirectory())
-            .map(dirent => dirent.name)
-            .filter(name => !name.startsWith('.'))
-
-        cliSelect({ values: ['root', ...childDirs], outputStream: process.stderr }).then(x => {
-                if (x.value === 'root') {
-                    console.log(bot)
-                    return
-                }
-                console.log(path.join(bot, x.value))
-            }).catch(() => {
-                console.log('.')
-                return
-            })
-    "
-    cd $(node -e $script)
-}
-
-htmlize() {
-    script="
-    const readline = require('readline')
-    const Convert = require('ansi-to-html')
-    const child_process = require('child_process')
-
-    const convert = new Convert({ bg: 'black', newline: true, escapeXML: true })
-
-    console.log(process.argv)
-    const argv = process.argv.slice(1)
-    if (argv.length < 1) {
-      console.log(\"The program's entry point must be the first positional param.\")
-      process.exit(1)
-    }
-    const programPath = argv[0]
-    console.log('About to run program: ' + programPath)
-
-    const p = child_process.spawn('node', [programPath, ...argv.slice(1)], {
-      env: {
-        ...process.env,
-        FORCE_COLOR: 1
-      }
-    })
-
-    var rl = readline.createInterface({
-      input: p.stdout,
-      output: process.stdout,
-      terminal: false
-    })
-
-    console.log('<pre style=\"background:black; width: fit-content;\">')
-    rl.on('line', (line) => {
-      const str = convert.toHtml(line)
-      console.log(str)
-    }).on('close', () => {
-      console.log('</pre>')
-      process.exit(0)
-    })"
-    node -e $script $@
-}
-
-current_pkg() {
+pkgname() {
+  dir=$1
   package_json="package.json"
-  if test -f "$package_json"; then
-      script="
+  fullpath=$(normalize "$dir/$package_json")
+  if test -f $fullpath; then
+      nodejs_script="
         try {
-          const fileContent = require('fs').readFileSync('$package_json');
+          const fileContent = require('fs').readFileSync('$fullpath');
           console.log(JSON.parse(fileContent).name)
         } catch {}
       "
-      pkg_name=$(node -e "$script")
+      pkg_name=$(node -e "$nodejs_script")
       echo $pkg_name
   fi
+}
+
+normalize() {
+  patharg=$1
+  nodejs_script="
+        try {
+          const path = require('path')
+          const normalized = path.normalize('$patharg')
+          console.log(normalized)
+        } catch {}
+      "
+  node -e $nodejs_script
 }
 
 yb() {
@@ -118,11 +62,11 @@ yws() {
 }
 
 duck() {
-    docker run -it --rm -p 8000:8000 --name docker-duckling rasa/duckling
+    docker run -it --rm -p 8000:8000 --name duckling rasa/duckling
 }
 
 redis() {
-    docker run -it --rm -p 6379:6379 --name docker-redis redis
+    docker run -it --rm -p 6379:6379 --name redis redis
 }
 
 getport() {
@@ -140,27 +84,6 @@ killport() {
     fi
     
     kill -9 $pid
-}
-
-bpconf() {
-    if [[ $# -eq 0 ]]
-    then
-        
-        pkg=$(current_pkg)
-        if [[ $pkg = $bp_main_pkg ]] then
-            echo "packages/bp/dist/data/global/botpress.config.json"
-        elif [[ $pkg = $bp_core_pkg ]] then
-            echo "dist/data/global/botpress.config.json"
-        else
-            echo "not in a kown botpress package: $pkg"
-        fi
-
-    elif [[ $1 = "zsh" ]]
-    then
-        echo "$HOME/.oh-my-zsh/custom/plugins/bp/bp.plugin.zsh"
-    else
-        echo "\"$1\" no such config file..."
-    fi
 }
 
 bpsql() {
--- readme.md
+++ readme.md
@@ -1,4 +1,8 @@
-to install, 
+### Description
 
-navigate to `~/.oh-my-zsh/custom/plugins` 
+Personal zsh plugin I use when coding on [Botpress](https://github.com/botpress/botpress).
+
+### Installation
+
+navigate to `~/.oh-my-zsh/custom/plugins`
 and clone this project using the command `git clone git@github.com:franklevasseur/botpress-omz-plugin.git bp`
